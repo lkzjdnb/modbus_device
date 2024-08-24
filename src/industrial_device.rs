2d8b69950a47e67264a5fbb78e00ab24c60593e9
leo <leo.lemaire007@orange.fr>
2024-08-24 10:50:40 +0200

    Conversion to industrial_device::types::Value

--- src/industrial_device.rs
+++ src/industrial_device.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use crate::modbus_connexion_async::ModbusConnexionAsync;
 use crate::modbus_device_async::ModbusDeviceAsync;
+use crate::types::RegisterValue;
 
 use industrial_device::types::Value;
 use industrial_device::IndustrialDevice;
@@ -18,5 +19,21 @@ impl IndustrialDevice for ModbusDeviceAsync {
         &mut self,
     ) -> Result<HashMap<String, Value>, Box<dyn std::error::Error + Send>> {
         todo!()
+    }
+}
+
+impl From<RegisterValue> for Value {
+    fn from(value: RegisterValue) -> Self {
+        match value {
+            RegisterValue::U16(val) => Value::U16(val),
+            RegisterValue::U32(val) => Value::U32(val),
+            RegisterValue::U64(val) => Value::U64(val),
+            RegisterValue::U128(val) => Value::U128(val),
+            RegisterValue::S32(val) => Value::S32(val),
+            RegisterValue::Enum16(val) => Value::Enum16(val),
+            RegisterValue::Sized(val) => Value::Sized(val),
+            RegisterValue::Float32(val) => Value::Float32(val),
+            RegisterValue::Boolean(val) => Value::Boolean(val),
+        }
     }
 }
